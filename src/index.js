5bdf62fc6bcb0f4d9cb34e4777951c063a621f36
Burkey Koontz <bk.koontz@gmail.com>
2018-09-09 09:14:46 -0700

    attempting to run on heroku

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CheeseList from './components/cheese-list';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+import {cheeseReducer} from './reducers/cheese';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cheeseList = [
+  "Bath Blue",
+  "Barkham Blue",
+  "Buxton Blue"
+];
+
+const store = createStore(cheeseReducer, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><CheeseList /></Provider>, document.getElementById('root'));
 registerServiceWorker();
